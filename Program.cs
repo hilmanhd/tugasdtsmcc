d876a6d789ac0dc2e7d7b33f7f1b1f6f3f6eb939
hilmanhd <100355032+hilmanhd@users.noreply.github.com>
2022-09-16 17:08:25 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DTSMCC
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int jamkedatangan;
+            jamkedatangan = 9;
+            if (jamkedatangan <= 8)
+            {
+                Console.WriteLine("Anda Tidak Terlambat Masuk Kantor!");
+            }
+            else
+            {
+                Console.WriteLine("Anda Terlambat Masuk Kantor!");
+            }
+        }
+
+    }
+}
